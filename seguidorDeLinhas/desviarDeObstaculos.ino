5ba9defcd70b2814b5f17c5b025ce18dae55d61b
Estenio Gabriel <esteniogabrieldelima@gmail.com>
2025-05-27 12:19:11 -0300

    servoMotor

--- seguidorDeLinhas/desviarDeObstaculos.ino
+++ seguidorDeLinhas/desviarDeObstaculos.ino
@@ -1,20 +1,44 @@
+// Micro Servo 
+#include <Servo.h>
+#define pinServo 11
+Servo servo;
 
+// Motores
 int motorEsquerdo1 = 6;
 int motorEsquerdo2 = 5;
 int motorDireito1 = 9;
 int motorDireito2 = 10;
-//
+
+
+// Funções // 
+  
+  //Servo Função
+  long moveServo(){
+    servo.write(100);
+    delay(500);
+    servo.write(180);
+    delay(500);
+    servo.write(100);
+    delay(500);
+    servo.write(30);
+    delay(500);
+  }
+  ////
 
 void setup() {
-  // put your setup code here, to run once:
-
+  servo.attach(pinServo);
+  
+  
 }
 
 void loop() {
-  analogWrite(motorEsquerdo1, 155);
-  digitalWrite(motorEsquerdo2, LOW);
 
-  analogWrite(motorDireito1, 155);
-  digitalWrite(motorDireito2, LOW);
+  
+  // analogWrite(motorEsquerdo1, 155);
+  // digitalWrite(motorEsquerdo2, LOW);
+
+  // analogWrite(motorDireito1, 155);
+  // digitalWrite(motorDireito2, LOW);
 
 }
+
